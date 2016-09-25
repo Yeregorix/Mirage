95265fe95d7c1493a52c0b081e5c757b1d6f5fac
thomas15v <tvanmellaerts@gmail.com>
2016-09-25 16:55:30 +0200

    Fixed problem with pistons

--- src/main/java/com/thomas15v/noxray/event/PlayerEventListener.java
+++ src/main/java/com/thomas15v/noxray/event/PlayerEventListener.java
@@ -3,6 +3,7 @@ package com.thomas15v.noxray.event;
 import org.spongepowered.api.block.BlockSnapshot;
 import org.spongepowered.api.block.BlockTypes;
 import org.spongepowered.api.data.Transaction;
+import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.block.ChangeBlockEvent;
 import org.spongepowered.api.util.Direction;
@@ -13,7 +14,9 @@ public class PlayerEventListener {
 
     @Listener
     public void onBlockUpdate(ChangeBlockEvent.Break event){
-        event.getTransactions().forEach(PlayerEventListener::updateBlocks);
+        if (event.getCause().containsType(Player.class)) {
+            event.getTransactions().forEach(PlayerEventListener::updateBlocks);
+        }
     }
 
     private static void updateBlocks(Transaction<BlockSnapshot> transaction){
