33f1378d7e14415befbc6d4d94473c073a6f4be1
Yeregorix <yeregorixcraft@gmail.com>
2018-03-03 19:53:12 +0100

    Improve bStats's MetricsLite

--- src/main/java/net/smoofyuniverse/antixray/bstats/MetricsLite.java
+++ src/main/java/net/smoofyuniverse/antixray/bstats/MetricsLite.java
@@ -7,19 +7,23 @@ import ninja.leaping.configurate.commented.CommentedConfigurationNode;
 import ninja.leaping.configurate.hocon.HoconConfigurationLoader;
 import org.apache.commons.lang3.Validate;
 import org.slf4j.Logger;
+import org.spongepowered.api.Game;
 import org.spongepowered.api.Platform;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.config.ConfigDir;
 import org.spongepowered.api.plugin.PluginContainer;
-import org.spongepowered.api.scheduler.Scheduler;
 import org.spongepowered.api.scheduler.Task;
 
 import javax.net.ssl.HttpsURLConnection;
-import java.io.*;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 import java.util.zip.GZIPOutputStream;
 
 /**
@@ -56,12 +60,9 @@ public class MetricsLite {
 	// The instance is created using Dependency Injection (https://docs.spongepowered.org/master/en/plugin/injection.html)
 	@Inject
 	private MetricsLite(PluginContainer plugin, Logger logger, @ConfigDir(sharedRoot = true) Path configDir) {
-		if (created) {
-			// We don't want more than one instance of this class
+		if (created)
 			throw new IllegalStateException("There's already an instance of this Metrics class!");
-		} else {
-			created = true;
-		}
+		created = true;
 
 		this.plugin = plugin;
 		this.logger = logger;
@@ -71,20 +72,20 @@ public class MetricsLite {
 			loadConfig();
 		} catch (IOException e) {
 			// Failed to load configuration
-			logger.warn("Failed to load bStats config!", e);
+			this.logger.warn("Failed to load bStats config!", e);
 			return;
 		}
 
 		// We are not allowed to send data about this server :(
-		if (!enabled) {
+		if (!this.enabled)
 			return;
-		}
 
 		Class<?> usedMetricsClass = getFirstBStatsClass();
 		if (usedMetricsClass == null) {
 			// Failed to get first metrics class
 			return;
 		}
+
 		if (usedMetricsClass == getClass()) {
 			// We are the first! :)
 			linkMetrics(this);
@@ -94,9 +95,8 @@ public class MetricsLite {
 			try {
 				usedMetricsClass.getMethod("linkMetrics", Object.class).invoke(null, this);
 			} catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-				if (logFailedRequests) {
-					logger.warn("Failed to link to first metrics class {}!", usedMetricsClass.getName(), e);
-				}
+				if (this.logFailedRequests)
+					this.logger.warn("Failed to link to first metrics class {}!", usedMetricsClass.getName(), e);
 			}
 		}
 	}
@@ -107,14 +107,15 @@ public class MetricsLite {
 	 * @throws IOException If something did not work :(
 	 */
 	private void loadConfig() throws IOException {
-		Path configPath = configDir.resolve("bStats");
-		configPath.toFile().mkdirs();
-		File configFile = new File(configPath.toFile(), "config.conf");
-		HoconConfigurationLoader configurationLoader = HoconConfigurationLoader.builder().setFile(configFile).build();
+		Path configPath = this.configDir.resolve("bStats"), configFile = configPath.resolve("config.conf");
+		Files.createDirectories(configPath);
+		HoconConfigurationLoader configLoader = HoconConfigurationLoader.builder().setPath(configFile).build();
+
 		CommentedConfigurationNode node;
-		if (!configFile.exists()) {
-			configFile.createNewFile();
-			node = configurationLoader.load();
+		if (Files.exists(configFile)) {
+			node = configLoader.load();
+		} else {
+			node = configLoader.createEmptyNode();
 
 			// Add default values
 			node.getNode("enabled").setValue(true);
@@ -125,21 +126,19 @@ public class MetricsLite {
 
 			// Add information about bStats
 			node.getNode("enabled").setComment(
-					"bStats collects some data for plugin authors like how many servers are using their plugins.\n" +
-							"To honor their work, you should not disable it.\n" +
-							"This has nearly no effect on the server performance!\n" +
-							"Check out https://bStats.org/ to learn more :)"
+					"bStats collects some data for plugin authors like how many servers are using their plugins.\n"
+							+ "To honor their work, you should not disable it.\n"
+							+ "This has nearly no effect on the server performance!\n"
+							+ "Check out https://bStats.org/ to learn more :)"
 			);
 
-			configurationLoader.save(node);
-		} else {
-			node = configurationLoader.load();
+			configLoader.save(node);
 		}
 
 		// Load configuration
-		enabled = node.getNode("enabled").getBoolean(true);
-		serverUUID = node.getNode("serverUuid").getString();
-		logFailedRequests = node.getNode("logFailedRequests").getBoolean(false);
+		this.enabled = node.getNode("enabled").getBoolean(true);
+		this.serverUUID = node.getNode("serverUuid").getString();
+		this.logFailedRequests = node.getNode("logFailedRequests").getBoolean();
 	}
 
 	/**
@@ -148,25 +147,28 @@ public class MetricsLite {
 	 * @return The first bStats metrics class.
 	 */
 	private Class<?> getFirstBStatsClass() {
-		Path configPath = configDir.resolve("bStats");
-		configPath.toFile().mkdirs();
-		File tempFile = new File(configPath.toFile(), "temp.txt");
+		Path configPath = this.configDir.resolve("bStats"), tempFile = configPath.resolve("temp.txt");
 
 		try {
-			String className = readFile(tempFile);
-			if (className != null) {
-				try {
-					// Let's check if a class with the given name exists.
-					return Class.forName(className);
-				} catch (ClassNotFoundException ignored) {
+			Files.createDirectories(configPath);
+
+			if (Files.exists(tempFile)) {
+				Optional<String> className = Files.lines(tempFile).findFirst();
+				if (className.isPresent()) {
+					try {
+						// Let's check if a class with the given name exists.
+						return Class.forName(className.get());
+					} catch (ClassNotFoundException ignored) {
+					}
 				}
 			}
-			writeFile(tempFile, getClass().getName());
+
+			Files.write(tempFile, Arrays.asList(getClass().getName(), "Note: This class only exists for internal purpose. You can ignore it :)"));
+
 			return getClass();
 		} catch (IOException e) {
-			if (logFailedRequests) {
-				logger.warn("Failed to get first bStats class!", e);
-			}
+			if (this.logFailedRequests)
+				this.logger.warn("Failed to get first bStats class!", e);
 			return null;
 		}
 	}
@@ -182,82 +184,34 @@ public class MetricsLite {
 	}
 
 	private void startSubmitting() {
-		// We use a timer cause want to be independent from the server tps
-		final Timer timer = new Timer(true);
-		timer.scheduleAtFixedRate(new TimerTask() {
-			@Override
-			public void run() {
-				// Plugin was disabled, e.g. because of a reload (is this even possible in Sponge?)
-				if (!Sponge.getPluginManager().isLoaded(plugin.getId())) {
-					timer.cancel();
-					return;
-				}
-				// The data collection (e.g. for custom graphs) is done sync
-				// Don't be afraid! The connection to the bStats server is still async, only the stats collection is sync ;)
-				Scheduler scheduler = Sponge.getScheduler();
-				Task.Builder taskBuilder = scheduler.createTaskBuilder();
-				taskBuilder.execute(() -> submitData()).submit(plugin);
-			}
-		}, 1000 * 60 * 5, 1000 * 60 * 30);
 		// Submit the data every 30 minutes, first time after 5 minutes to give other plugins enough time to start
 		// WARNING: Changing the frequency has no effect but your plugin WILL be blocked/deleted!
-		// WARNING: Just don't do it!
-	}
 
-	/**
-	 * Reads the first line of the file.
-	 *
-	 * @param file The file to read. Cannot be null.
-	 * @return The first line of the file or <code>null</code> if the file does not exist or is empty.
-	 * @throws IOException If something did not work :(
-	 */
-	private String readFile(File file) throws IOException {
-		if (!file.exists()) {
-			return null;
-		}
-		try (
-				FileReader fileReader = new FileReader(file);
-				BufferedReader bufferedReader = new BufferedReader(fileReader)
-		) {
-			return bufferedReader.readLine();
-		}
-	}
+		Task.builder().execute(t -> {
+			if (Sponge.getPluginManager().isLoaded(this.plugin.getId())) {
+				// The data collection (e.g. for custom graphs) is done sync
+				// Don't be afraid! The connection to the bStats server is still async, only the stats collection is sync ;)
 
-	/**
-	 * Writes a String to a file. It also adds a note for the user,
-	 *
-	 * @param file The file to write to. Cannot be null.
-	 * @param text The text to write.
-	 * @throws IOException If something did not work :(
-	 */
-	private void writeFile(File file, String text) throws IOException {
-		if (!file.exists()) {
-			file.createNewFile();
-		}
-		try (
-				FileWriter fileWriter = new FileWriter(file);
-				BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)
-		) {
-			bufferedWriter.write(text);
-			bufferedWriter.newLine();
-			bufferedWriter.write("Note: This class only exists for internal purpose. You can ignore it :)");
-		}
+				submitData();
+			} else {
+				t.cancel();
+			}
+		}).delay(5, TimeUnit.MINUTES).interval(30, TimeUnit.MINUTES).submit(this.plugin);
 	}
 
 	/**
 	 * Collects the data and sends it afterwards.
 	 */
 	private void submitData() {
-		final JsonObject data = getServerData();
+		JsonObject data = getServerData();
 
 		JsonArray pluginData = new JsonArray();
 		// Search for all other bStats Metrics classes to get their plugin data
 		for (Object metrics : knownMetricsInstances) {
 			try {
 				Object plugin = metrics.getClass().getMethod("getPluginData").invoke(metrics);
-				if (plugin instanceof JsonObject) {
+				if (plugin instanceof JsonObject)
 					pluginData.add((JsonObject) plugin);
-				}
 			} catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException ignored) {
 			}
 		}
@@ -265,17 +219,16 @@ public class MetricsLite {
 		data.add("plugins", pluginData);
 
 		// Create a new thread for the connection to the bStats server
-		new Thread(() -> {
+		Task.builder().async().execute(() -> {
 			try {
 				// Send the data
 				sendData(data);
 			} catch (Exception e) {
 				// Something went wrong! :(
-				if (logFailedRequests) {
-					logger.warn("Could not submit plugin stats!", e);
-				}
+				if (this.logFailedRequests)
+					this.logger.warn("Could not submit plugin stats!", e);
 			}
-		}).start();
+		}).submit(this.plugin);
 	}
 
 	/**
@@ -284,34 +237,24 @@ public class MetricsLite {
 	 * @return The server specific data.
 	 */
 	private JsonObject getServerData() {
-		// Minecraft specific data
-		int playerAmount = Sponge.getServer().getOnlinePlayers().size();
-		playerAmount = playerAmount > 200 ? 200 : playerAmount;
-		int onlineMode = Sponge.getServer().getOnlineMode() ? 1 : 0;
-		String minecraftVersion = Sponge.getGame().getPlatform().getMinecraftVersion().getName();
-		String spongeImplementation = Sponge.getPlatform().getContainer(Platform.Component.IMPLEMENTATION).getName();
-
-		// OS/Java specific data
-		String javaVersion = System.getProperty("java.version");
-		String osName = System.getProperty("os.name");
-		String osArch = System.getProperty("os.arch");
-		String osVersion = System.getProperty("os.version");
-		int coreCount = Runtime.getRuntime().availableProcessors();
-
+		Game game = Sponge.getGame();
 		JsonObject data = new JsonObject();
 
 		data.addProperty("serverUUID", serverUUID);
 
-		data.addProperty("playerAmount", playerAmount);
-		data.addProperty("onlineMode", onlineMode);
-		data.addProperty("minecraftVersion", minecraftVersion);
-		data.addProperty("spongeImplementation", spongeImplementation);
+		// Minecraft specific data
+		int players = game.getServer().getOnlinePlayers().size();
+		data.addProperty("playerAmount", players > 200 ? 200 : players);
+		data.addProperty("onlineMode", game.getServer().getOnlineMode() ? 1 : 0);
+		data.addProperty("minecraftVersion", game.getPlatform().getMinecraftVersion().getName());
+		data.addProperty("spongeImplementation", game.getPlatform().getContainer(Platform.Component.IMPLEMENTATION).getName());
 
-		data.addProperty("javaVersion", javaVersion);
-		data.addProperty("osName", osName);
-		data.addProperty("osArch", osArch);
-		data.addProperty("osVersion", osVersion);
-		data.addProperty("coreCount", coreCount);
+		// OS/Java specific data
+		data.addProperty("javaVersion", System.getProperty("java.version"));
+		data.addProperty("osName", System.getProperty("os.name"));
+		data.addProperty("osArch", System.getProperty("os.arch"));
+		data.addProperty("osVersion", System.getProperty("os.version"));
+		data.addProperty("coreCount", Runtime.getRuntime().availableProcessors());
 
 		return data;
 	}
@@ -324,28 +267,28 @@ public class MetricsLite {
 	 */
 	private static void sendData(JsonObject data) throws Exception {
 		Validate.notNull(data, "Data cannot be null");
-		HttpsURLConnection connection = (HttpsURLConnection) new URL(URL).openConnection();
+		HttpsURLConnection co = (HttpsURLConnection) new URL(URL).openConnection();
 
 		// Compress the data to save bandwidth
-		byte[] compressedData = compress(data.toString());
+		byte[] bytes = compress(data.toString());
 
 		// Add headers
-		connection.setRequestMethod("POST");
-		connection.addRequestProperty("Accept", "application/json");
-		connection.addRequestProperty("Connection", "close");
-		connection.addRequestProperty("Content-Encoding", "gzip"); // We gzip our request
-		connection.addRequestProperty("Content-Length", String.valueOf(compressedData.length));
-		connection.setRequestProperty("Content-Type", "application/json"); // We send our data in JSON format
-		connection.setRequestProperty("User-Agent", "MC-Server/" + B_STATS_VERSION);
+		co.setRequestMethod("POST");
+		co.addRequestProperty("Accept", "application/json");
+		co.addRequestProperty("Connection", "close");
+		co.addRequestProperty("Content-Encoding", "gzip"); // We gzip our request
+		co.addRequestProperty("Content-Length", String.valueOf(bytes.length));
+		co.setRequestProperty("Content-Type", "application/json"); // We send our data in JSON format
+		co.setRequestProperty("User-Agent", "MC-Server/" + B_STATS_VERSION);
 
 		// Send data
-		connection.setDoOutput(true);
-		DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
-		outputStream.write(compressedData);
-		outputStream.flush();
-		outputStream.close();
+		co.setDoOutput(true);
+		DataOutputStream out = new DataOutputStream(co.getOutputStream());
+		out.write(bytes);
+		out.flush();
+		out.close();
 
-		connection.getInputStream().close(); // We don't care about the response - Just send our data :)
+		co.getInputStream().close(); // We don't care about the response - Just send our data :)
 	}
 
 	/**
@@ -355,15 +298,12 @@ public class MetricsLite {
 	 * @return The gzipped String.
 	 * @throws IOException If the compression failed.
 	 */
-	private static byte[] compress(final String str) throws IOException {
-		if (str == null) {
-			return null;
-		}
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		GZIPOutputStream gzip = new GZIPOutputStream(outputStream);
+	private static byte[] compress(String str) throws IOException {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		GZIPOutputStream gzip = new GZIPOutputStream(out);
 		gzip.write(str.getBytes("UTF-8"));
 		gzip.close();
-		return outputStream.toByteArray();
+		return out.toByteArray();
 	}
 
 	/**
@@ -375,30 +315,10 @@ public class MetricsLite {
 	public JsonObject getPluginData() {
 		JsonObject data = new JsonObject();
 
-		String pluginName = plugin.getName();
-		String pluginVersion = plugin.getVersion().orElse("unknown");
-
-		data.addProperty("pluginName", pluginName);
-		data.addProperty("pluginVersion", pluginVersion);
-
-		JsonArray customCharts = new JsonArray();
-		data.add("customCharts", customCharts);
+		data.addProperty("pluginName", this.plugin.getName());
+		data.addProperty("pluginVersion", this.plugin.getVersion().orElse("unknown"));
+		data.add("customCharts", new JsonArray());
 
 		return data;
 	}
-
-	static {
-		// You can use the property to disable the check in your test environment
-		if (System.getProperty("bstats.relocatecheck") == null || !System.getProperty("bstats.relocatecheck").equals("false")) {
-			// Maven's Relocate is clever and changes strings, too. So we have to use this little "trick" ... :D
-			final String defaultPackage = new String(
-					new byte[]{'o', 'r', 'g', '.', 'b', 's', 't', 'a', 't', 's', '.', 's', 'p', 'o', 'n', 'g', 'e'});
-			final String examplePackage = new String(new byte[]{'y', 'o', 'u', 'r', '.', 'p', 'a', 'c', 'k', 'a', 'g', 'e'});
-			// We want to make sure nobody just copy & pastes the example and use the wrong package names
-			if (MetricsLite.class.getPackage().getName().equals(defaultPackage) || MetricsLite.class.getPackage().getName().equals(examplePackage)) {
-				throw new IllegalStateException("bStats Metrics class has not been relocated correctly!");
-			}
-		}
-	}
-
 }
